d23e31276b686975894e6fa2c22f8dcd703ad8f8
Dominik Rasiński <dox.gmr@gmail.com>
2025-05-03 16:14:16 +0200

    feat: Implement Starship retrieval in StarshipApiController and add findAll method in StarshipRepository

--- starshop/src/Controller/StarshipApiController.php
+++ starshop/src/Controller/StarshipApiController.php
@@ -8,36 +8,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 use App\Model\Starship;
 use App\Repository\StarshipRepository;
+use Psr\Log\LoggerInterface;
 
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getColection(): Response
+    public function getCollection(LoggerInterface $logger, StarshipRepository $repository): Response
     {
-        $starships = [
-            new Starship(
-                1,
-                'USS LeafyCruiser (NCC-0001)',
-                'Garden',
-                'Jean-Luc Pickles',
-                'taken over by Q'
-            ),
-            new Starship(
-                2,
-                'USS Espresso (NCC-1234-C)',
-                'Latte',
-                'James T. Quick!',
-                'repaired',
-            ),
-            new Starship(
-                3,
-                'USS Wanderlust (NCC-2024-W)',
-                'Delta Tourist',
-                'Kathryn Journeyway',
-                'under construction',
-            ),
-        ];
-
+        $logger->info('Starship collection retrieved');
+        dd($repository);
         return $this->json($starships);
     }
 }
